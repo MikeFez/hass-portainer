ed6ae6f762a2d02b204b798f5963389bea663b23
Michael Fessenden <MikeFez@gmail.com>
2020-01-17 01:21:44 -0500

    Initial implementation of portainer integration

--- custom_component/portainer/__init__.py
+++ custom_component/portainer/__init__.py
@@ -1,10 +1,188 @@
 """Portainer Platform Integration"""
-PORTAINER = 'portainer'
+from datetime import timedelta
+import logging
+from typing import Sequence, TypeVar, Union
+import json
+import homeassistant.helpers.config_validation as cv
+import requests
+import voluptuous as vol
+from homeassistant.helpers.entity import Entity
 
+DOMAIN = 'portainer'
+
+MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
+
+CONF_HOST = "host"
+CONF_PORT = "port"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_FILTER = "filter"
+CONF_FILTER_ENDPOINT = "endpoint"
+CONF_FILTER_ENDPOINT_ENABLED = "enabled"
+CONF_FILTER_INCLUDE_CONTAINERS = "include_containers"
+CONF_FILTER_EXCLUDE_CONTAINERS = "exclude_containers"
+
+T = TypeVar("T")  # pylint: disable=invalid-name
+
+# This version of ensure_list interprets an empty dict as no value
+def ensure_list(value: Union[T, Sequence[T]]) -> Sequence[T]:
+    """Wrap value in list if it is not one."""
+    if value is None or (isinstance(value, dict) and not value):
+        return []
+    return value if isinstance(value, list) else [value]
+
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.All(
+            ensure_list,
+            [
+                vol.Schema(
+                    {
+                        vol.Required(CONF_HOST): cv.url,
+                        vol.Required(CONF_USERNAME): cv.string,
+                        vol.Required(CONF_PASSWORD): cv.string,
+                        vol.Optional(CONF_PORT, default=''): cv.string,
+                        vol.Optional(CONF_FILTER, default=[]): vol.All(
+                            ensure_list,
+                            [
+                                vol.Schema(
+                                    {
+                                        vol.Required(CONF_FILTER_ENDPOINT): cv.string,
+                                        vol.Optional(CONF_FILTER_ENDPOINT_ENABLED, default=True): cv.boolean,
+                                        vol.Optional(CONF_FILTER_INCLUDE_CONTAINERS, default=[]): cv.ensure_list,
+                                        vol.Optional(CONF_FILTER_EXCLUDE_CONTAINERS, default=[]): cv.ensure_list,
+                                    }
+                                )
+                            ],
+                        )
+                    }
+                )
+            ],
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+class PortainerHost():
+    """Implementation of an Portainer sensor"""
+    def __init__(self, host, port, username, password, endpoint_filter):
+        """Initialize the sensor"""
+        self._host = host
+        self._port = port
+        self._username = username
+        self._password = password
+        self._endpoint_filter = endpoint_filter
+        
+        self._name = f"portainer_{host}"
+        self._api_url = f"{host}{':' + port if port else ''}/api"
+        self._api_token = None
+        
+        self.endpoints = []
+        
+        self.refresh_authentication_token()
+        self.refresh_endpoints()
+        
+    def refresh_authentication_token(self):
+        logging.debug("Refreshing API Authentication Token")
+        res = requests.post(f"{self._api_url}/auth", json={"Username": self._username, "Password": self._password})
+        res.raise_for_status()
+        self._api_token = "Bearer " + res.json()['jwt']
+        return
+        
+    def refresh_endpoints(self):
+        logging.debug("Gathering list of endpoints")
+        res = requests.get(f"{self._api_url}/endpoints", headers={"Authorization": self._api_token})
+        res.raise_for_status()
+             
+        endpoints = []
+        for endpoint_json in res.json():
+            endpoint = Endpoint(self, endpoint_json)
+            endpoint_filter = self._endpoint_filter.get(endpoint._name, None)
+            if endpoint_filter and not endpoint_filter[CONF_FILTER_ENDPOINT_ENABLED]:
+                continue
+            else:
+                endpoint._container_filter = endpoint_filter
+                endpoint.refresh_containers()
+                endpoints.append(endpoint)
+        self.endpoints = endpoints
+        return
+
+class Endpoint(Entity):
+    """Implementation of an Portainer sensor"""
+    def __init__(self, PortainerHost, endpoint_json):
+        self._portainer_host = PortainerHost
+        self._id = endpoint_json['Id']
+        self._name = endpoint_json['Name']
+        self._status = endpoint_json['Status']
+        self._container_filter = self._portainer_host._endpoint_filter.get(self._name, None)
+        self._containers = []
+
+    class Container():
+        def __init__(self, container_json):
+            self._id = container_json['Id']
+            self._name = container_json['Names'][0][1:]
+            self._image = container_json['Image']
+            self._status = container_json['Status']
+            self._state = container_json['State']
+    
+    @property
+    def name(self):
+        """Return the name of the sensor"""
+        return f"portainer_{self._name}"
+
+    @property
+    def state(self):
+        """Return the next arrival time"""
+        return "Online"
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes """
+        logging.debug("Returing attributes")
+        attributes = {}
+        for container in self._containers:
+            attributes[container._name] = container._state
+        return attributes
+        
+    def refresh_containers(self):
+        logging.info(f"Gathering list of containers within {self._name}")
+        res = requests.get(f"{self._portainer_host._api_url}/endpoints/{self._id}/docker/containers/json", headers={"Authorization": self._portainer_host._api_token})
+        res.raise_for_status()
+        containers = []
+        for container_json in res.json():
+            container = Endpoint.Container(container_json)
+            if self._container_filter:
+                if container._name in self._container_filter[CONF_FILTER_EXCLUDE_CONTAINERS]:
+                    continue
+                if self._container_filter[CONF_FILTER_INCLUDE_CONTAINERS] and container._name not in self._container_filter[CONF_FILTER_INCLUDE_CONTAINERS]:
+                    continue
+            containers.append(container)  
+        self._containers = containers
+        return
+
+    def update(self):
+        """Get the latest data and update the state."""
+        try:
+            self.refresh_containers()
+        except Exception as e:
+            logging.exception(f"Encountered Exception: {e}")        
 
 def setup(hass, config):
-    """Setup sensors ."""
+    """Setup hosts"""
     # Data that you want to share with your platforms
-    hass.data[PORTAINER] = {}
-    hass.helpers.discovery.load_platform('sensor', PORTAINER, {}, config)
+    hosts = []
+    for portainer_configuration in config.get(DOMAIN):
+        organized_endpoint_filter = {}
+        for endpoint_filter in portainer_configuration.get(CONF_FILTER):
+            organized_endpoint_filter[endpoint_filter.get(CONF_FILTER_ENDPOINT)] = endpoint_filter
+        host = PortainerHost(host=portainer_configuration.get(CONF_HOST),
+                               port=portainer_configuration.get(CONF_PORT),
+                               username=portainer_configuration.get(CONF_USERNAME),
+                               password=portainer_configuration.get(CONF_PASSWORD),
+                               endpoint_filter=organized_endpoint_filter)
+        hosts.append(host)
+        
+    hass.data[DOMAIN] = {"hosts": hosts} 
+    hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
     return True
--- custom_component/portainer/sensor.py
+++ custom_component/portainer/sensor.py
@@ -3,135 +3,19 @@ import logging
 import json
 import homeassistant.helpers.config_validation as cv
 import requests
-import voluptuous as vol
-from dateutil.relativedelta import relativedelta
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_NAME
-from homeassistant.helpers.entity import Entity
 
-from . import PORTAINER
+from . import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_INSTANCES = "instances"
-CONF_HOST = "host"
-CONF_PORT = "port"
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
-CONF_INCLUDE_ENDPOINTS = "include_endpoints"
-CONF_EXCLUDE_ENDPOINTS = "exclude_endpoints"
-
 SCAN_INTERVAL = timedelta(seconds=30)
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {
-        vol.Optional(CONF_INSTANCES): [
-            {
-                vol.Required(CONF_HOST): cv.url,
-                vol.Required(CONF_USERNAME): cv.string,
-                vol.Required(CONF_PASSWORD): cv.string,
-                vol.Optional(CONF_PORT, default=''): cv.string,
-                vol.Optional(CONF_INCLUDE_ENDPOINTS, default=[]): cv.ensure_list,
-                vol.Optional(CONF_EXCLUDE_ENDPOINTS, default=[]): cv.ensure_list,
-            }
-        ]
-    }
-)
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the Volkswagen sensors."""    
+    """Set up the Endpoint sensors."""    
     sensors = []
-    for portainer_configuration in config.get(CONF_INSTANCES):
-        sensors.append(PortainerSensor(host=portainer_configuration.get(CONF_HOST),
-                                       port=portainer_configuration.get(CONF_PORT),
-                                       username=portainer_configuration.get(CONF_USERNAME),
-                                       password=portainer_configuration.get(CONF_PASSWORD),
-                                       include_endpoints=portainer_configuration.get(CONF_INCLUDE_ENDPOINTS),
-                                       exclude_endpoints=portainer_configuration.get(CONF_EXCLUDE_ENDPOINTS)))
+    for host in hass.data[DOMAIN]['hosts']:
+        for endpoint in host.endpoints:
+            sensors.append(endpoint)
     add_entities(sensors, True)
     return
-
-class Endpoint():
-    def __init__(endpoint_json):
-        self.id = endpoint_json['Id']
-        self.name = endpoint_json['Name']
-        self.status = endpoint_json['Status']
-        self.containers = []
-
-    class Container():
-        def __init__(container_json):
-            self.id = container_json['Id']
-            self.name = container_json['Names'][0][1:]
-            self.image = container_json['Image']
-            self.status = container_json['Status']
-            self.state = container_json['State']
-        
-class PortainerSensor(Entity):
-    """Implementation of an Portainer sensor"""
-    def __init__(self, host, port, username, password, include_endpoints, exclude_endpoints):
-        """Initialize the sensor"""
-        self._host = host
-        self._port = port
-        self._username = username
-        self._password = password
-        self._include_endpoints = include_endpoints
-        self._exclude_endpoints = exclude_endpoints
-        
-        self._name = f"portainer_{host}"
-        self._api_url = f"{host}{':' + port if port else ''}/api"
-        self._api_token = None
-        
-        self.endpoints = []
-        self.refresh_authentication_token()
-        
-    
-    @property
-    def name(self):
-        """Return the name of the sensor"""
-        return self._name
-
-    @property
-    def state(self):
-        """Return the next arrival time"""
-        return str(len(self.endpoints))
-
-    @property
-    def device_state_attributes(self):
-        """Return the state attributes """
-        logging.debug("Returing attributes")
-        attributes = {endpoint.name: son.dumps([container.name for container in endpoint.containers] for endpoint in self.endpoints)}
-        return attributes
-        
-    def refresh_authentication_token(self):
-        logging.debug("Refreshing API Authentication Token")
-        res = requests.post(f"{self._api_url}/auth", json={"Username": self._username, "Password": self._password})
-        res.raise_for_status()
-        self._api_token = "Bearer " + res.json()['jwt']
-        return
-        
-    def get_endpoints(self):
-        logging.debug("Gathering list of endpoints")
-        res = requests.get(f"{self._api_url}/endpoints", headers={"Authorization": self._api_token})
-        res.raise_for_status()
-        logging.warning(res.json())
-        endpoints = [Endpoint(endpoint_json) for endpoint_json in res.json()]
-        if self._include_endpoints:
-            endpoints = [endpoint for endpoint in endpoints if endpoint.name not in self._exclude_endpoints]
-        if self._include_endpoints:
-            endpoints = [endpoint for endpoint in endpoints if endpoint.name in self._include_endpoints]
-        return endpoints
-        
-    def get_containers(self, endpoint):
-        logging.info(f"Gathering list of containers within {endpoint['Name']}")
-        res = requests.get(f"{self._api_url}/endpoints/{endpoint.id}/docker/containers/json", headers={"Authorization": self._api_token})
-        res.raise_for_status()
-        containers = [Endpoint.Container(container_json) for container_json in res.json()]
-        return containers
-
-    def update(self):
-        """Get the latest data and update the state."""
-        try:
-            for endpoint in self.get_endpoints():
-                endpoint.get_containers = self.get_containers()
-        except Exception as e:
-            logging.exception(f"Encountered Exception: {e}")
